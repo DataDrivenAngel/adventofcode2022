ce62e057b7b44e57b65fa1ce91e43a05c130e4eb
DataDrivenAngel <buildingwill@gmail.com>
2022-12-05 21:05:00 -0500

    gold star!

--- 2/2.py
+++ 2/2.py
@@ -1,12 +1,24 @@
 text = open("2.txt",'r')
 input = text.read().split('\n')
-# scores
-win = 6
-draw = 3
-loss = 0
-shape_rock = 1
-shape_paper = 2
-shape_scissors = 3
+
+scores ={
+    "win"  : 6,
+    "draw" : 3,
+    "loss" : 0,
+    "R"    : 1,
+    "P"    : 2,
+    "S"    : 3,
+}
+
+moves = {
+    "A"    : "R",
+    "B"    : "P",
+    "C"    : "S",
+    "X"    : "R",
+    "Y"    : "P",
+    "Z"    : "S",
+}
+
 
 # rock > paper > scissors > rock
 # A > B > C > A
@@ -14,8 +26,31 @@ shape_scissors = 3
 # X = A
 # Y = B
 # Z = C
-round = input[0].split()
-elf = round[0]
-self = round[1]
-print(elf)
 
+total_score = 0
+for iter, rounds in enumerate(input):
+    round = input[iter].split()
+    elf = round[0]
+    self = round[1]
+    round_score = scores[moves[self]]
+    status = ""
+
+    if moves[self] == moves[elf]:
+        status = "draw"
+    elif moves[self] == "R" and moves[elf] == "P":
+        status = "loss"
+    elif moves[self] == "P" and moves[elf] == "S":
+        status = "loss"
+    elif moves[self] == "S" and moves[elf] == "R":
+        status = "loss"
+    else:
+        status = "win"
+
+    round_score = round_score + scores[status]
+
+
+    total_score = total_score + round_score
+   # print(iter, "|", round, "|",elf,self,"| ", "Elf played",scores[elf], " versus your", scores[self], "you", status, "| Round Score:",round_score,"Total Score:",total_score)
+print(total_score)
+
+# 10941 is correct!
